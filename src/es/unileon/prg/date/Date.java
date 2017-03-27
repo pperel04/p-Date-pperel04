00ad2b1214bff0ea92f357fe0903bfb86d921274
Pablo Pérez López <pperel04@estudiantes.unileon.es>
2017-03-27 12:24:38 +0200

    Acabados los set y los get de la clase Date

--- src/es/unileon/prg/date/Date.java
+++ src/es/unileon/prg/date/Date.java
@@ -2,4 +2,51 @@ package es.unileon.prg.date;
 
 /* Clase Date con los métodos dados en clase de teoría sobre sentencias de control */
 
+public class Date {
+	private int _day;
+	private int _month;
+	private int _year;
 
+
+	public Date(int day, int month, int year){
+		this._day = day;
+		this._month = month;
+		this._year = year;
+	}
+
+	public Date(){
+		_day = 6;
+		_month = 9;
+		_year = 2010;
+	}
+
+	public Date(Date another){
+		this._day = another.getDay();
+		this._month = another.getMonth();
+		this._year = another.getYear();
+	}
+
+	public int getDay(){
+		return _day;
+	}
+
+	public int getMonth(){
+		return _month;
+	}
+
+	public int getYear(){
+		return _year;
+	}
+
+	public void setDay(int day){
+		this._day = day;
+	}
+
+	public void setMonth(int month){
+		this._month =  month;
+	}
+
+	public void setYear(int year){
+		this._year = year;
+	}
+}
